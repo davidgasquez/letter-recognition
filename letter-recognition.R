c62c6f8c998c1f820c80ba3238dc39fe6c7ffe1c
David Gasquez <davidgasquez@gmail.com>
2015-04-08 11:40:50 +0200

    Add KNN prediction

--- letter-recognition.R
+++ letter-recognition.R
@@ -1,5 +1,6 @@
 # Loading libraries
 library(MASS)         # LDA and QDA
+library(class)        # KNN
 
 # Load data
 letter.recognition <- read.csv("letter-recognition.data")
@@ -47,34 +48,64 @@ lda2.test.rate <- mean(lda2.test.class == Letter[-train])
 ###############################################################################
 
 # Let's see with QDA
-qda.fit = qda(Letter~., letter.recognition[train,])
+qda.fit <- qda(Letter~., letter.recognition[train,])
 
 # Make prediction
-qda.test.pred = predict(qda.fit, letter.recognition[-train,])
-qda.test.class = qda.test.pred$class
+qda.test.pred <- predict(qda.fit, letter.recognition[-train,])
+qda.test.class <- qda.test.pred$class
 
 # Compute error
-qda.test.rate = mean(qda.test.class == Letter[-train])
+qda.test.rate <- mean(qda.test.class == Letter[-train])
 
 # Now we use combinations
-qda2.fit=qda(Letter~.*., letter.recognition[train,])
+qda2.fit <- qda(Letter~.*., letter.recognition[train,])
 
 #  Make again the prediction
-qda2.test.pred=predict(qda2.fit, letter.recognition[-train,])
-qda2.test.class=qda2.test.pred$class
+qda2.test.pred <- predict(qda2.fit, letter.recognition[-train,])
+qda2.test.class <- qda2.test.pred$class
 
 # Compute new error
-qda2.test.rate = mean(qda2.test.class==Letter[-train])
+qda2.test.rate <- mean(qda2.test.class==Letter[-train])
+
+###############################################################################
+#                                      KNN                                    #
+###############################################################################
+
+# Set labels
+label <- letter.recognition[train,1]
+
+# Explore different K values
+error.rate <- numeric(10)
+for(i in 1:10){
+  knn.pred <- knn(letter.recognition[train,-1], letter.recognition[-train,-1],label, k = i)
+  error.rate[i] <- 1-mean(knn.pred == letter.recognition[-train, 1])
+}
+
+# Plot error rates
+plot(1:10, error.rate,"b", pch = 20, col = "red", xlab = "K", ylab = "Error Rate")
+
+# Make prediction with K = 1
+knn.pred <- knn(letter.recognition[train,-1], letter.recognition[-train,-1], label, k = 1)
+
+# Save test rate
+knn.test.rate <- mean(knn.pred == letter.recognition[-train, 1])
+
+# Same with Cross Validation
+knn.cv.pred <- knn.cv(letter.recognition[,-1], letter.recognition[,1], k = 1)
+knn.cv.test.rate <- mean(knn.cv.pred == letter.recognition[, 1])
 
 
 ###############################################################################
 #                                 Results                                     #
 ###############################################################################
 
-# From LDA we have
+# LDA
 lda.test.rate
 lda2.test.rate
 
-# QDA is better than LDA
+# QDA
 qda.test.rate
 qda2.test.rate
+
+# KNN
+knn.cv.test.rate
