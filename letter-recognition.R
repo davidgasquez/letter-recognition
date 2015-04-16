a20acd2344ff4cf76ae7da5658203055442eead0
David Gasquez <davidgasquez@gmail.com>
2015-04-16 10:37:29 +0200

    Add random forest fit and results

--- letter-recognition.R
+++ letter-recognition.R
@@ -140,7 +140,7 @@ tree2.pred <- predict(tree2.fit, letter.recognition[,], type="class")
 tree2.test.rate <- mean(tree2.pred == letter.recognition[, 1])
 
 ###############################################################################
-#                                 Bagging                                     #
+#                                  Bagging                                    #
 ###############################################################################
 
 # Fit a bagging model
@@ -155,20 +155,99 @@ bagging.pred <- predict(bagging.fit,letter.recognition[-train,])
 bagging.test.rate <- mean(bagging.pred == letter.recognition[-train, 1])
 # It's almost OOB estimate error rate
 
+
+###############################################################################
+#                               Random Forest                                 #
+###############################################################################
+
+# Default fit
+randomForest.fit <- randomForest(Letter ~ ., data=letter.recognition[train, ])
+
+# Review the prefious fit
+randomForest.fit # 500 trees (4 lenght)
+
+# Let's see each variable importance
+importance(randomForest.fit)
+
+# Make a prediction
+randomForest.pred <- predict(randomForest.fit , letter.recognition[-train, ])
+
+# Test rate
+randomForest.test.rate <- mean(
+    randomForest.pred == letter.recognition[-train, 1])
+
+# Compute number of variables to select
+sqrt(dim(letter.recognition)[2]-1)
+
+# Make the fit with differents parameters
+randomForest2.fit <- randomForest(Letter ~ .,
+                                  data=letter.recognition[train, ],
+                                  ntree=100, mtry=4,
+                                  importance=TRUE)
+randomForest2.pred <- predict(randomForest2.fit , letter.recognition[-train, ])
+randomForest2.test.rate<- mean(
+    randomForest2.pred == letter.recognition[-train, 1])
+
+# Explore with other parameters to ensure optimum on 4
+randomForest3.fit <- randomForest(Letter ~ .,
+                                 data=letter.recognition[train, ],
+                                 ntree=500, mtry=5,
+                                 importance=TRUE)
+randomForest3.pred <- predict(randomForest3.fit , letter.recognition[-train, ])
+randomForest3.test.rate <- mean(
+    randomForest3.pred == letter.recognition[-train, 1])
+
+# Let's combine previous random forestsfits
+rf.1 <- randomForest(Letter ~ ., data=letter.recognition[train,])
+rf.2 <- randomForest(Letter ~ ., data=letter.recognition[train,])
+rf.3 <- randomForest(Letter ~ ., data=letter.recognition[train,])
+rf.all <- combine(rf.1, rf.2, rf.3)
+
+# Make prediction
+randomForest.c.pred <- predict(rf.all , letter.recognition[-train,])
+randomForest.c.test.rate <- mean(
+    randomForest.c.pred == letter.recognition[-train, 1])
+
+# Spin the training data
+train1 <- sample(1:nrow(letter.recognition[train, ]), 16000)
+rf1 <- randomForest(Letter ~ .*., data=letter.recognition[train1, ])
+train2 <- sample(1:nrow(letter.recognition[train, ]), 16000)
+rf2 <- randomForest(Letter ~ .*., data=letter.recognition[train2, ])
+train3 <- sample(1:nrow(letter.recognition[train, ]), 16000)
+rf3 <- randomForest(Letter ~ .*., data=letter.recognition[train3, ])
+train4 <- sample(1:nrow(letter.recognition[train, ]), 16000)
+rf4 <- randomForest(Letter ~ .*., data=letter.recognition[train4, ])
+train5 <- sample(1:nrow(letter.recognition[train, ]), 16000)
+rf5 <- randomForest(Letter ~ .*., data=letter.recognition[train5, ])
+
+rfall <- combine(rf1, rf2, rf3, rf4, rf5)
+
+# Make prediction
+randomForest.a.pred <- predict(rfall , letter.recognition[-train,])
+randomForest.a.test.rate <- mean(
+    randomForest.a.pred == letter.recognition[-train, 1])
+
+
 ###############################################################################
 #                                 Results                                     #
 ###############################################################################
 
 # LDA
-lda.test.rate
-lda2.test.rate
+print(lda.test.rate)
+print(lda2.test.rate)
 
 # QDA
-qda.test.rate
-qda2.test.rate
+print(qda.test.rate)
+print(qda2.test.rate)
 
 # KNN
-knn.cv.test.rate
+print(knn.cv.test.rate)
 
 # Tree(simple)
-tree2.test.rate
+print(tree2.test.rate)
+
+# Bagging
+print(bagging.test.rate)
+
+# Random Forests
+print(randomForest.a.test.rate)
